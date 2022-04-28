05292d070a64e20d54af0ccd5e7419f1d43f6bab
José Lumbo <joselumbo77@gmail.com>
2022-04-27 22:29:05 -0300

    Atualizando o arquivo hallocontroller

--- src/main/java/com/Halloword/hallo/controller/HalloController.java
+++ src/main/java/com/Halloword/hallo/controller/HalloController.java
@@ -16,9 +16,20 @@ public class HalloController {
 	}
 	@GetMapping("/bsm")
 	public String bsm() {
-		return "EstouLivre!!!";
-			
-		
+		return 
+				"BSM-0: Introdução às Habilidades Comportamentais e Mentalidades <br/ > BSM-GM: Mentalidade de Crescimento <br/ > BSM-P: Persistência <br/ > BSM-PR: Responsabilidade Pessoal \r\n"
+                + "<br/ > BSM-FO: Orientação ao Futuro <br/ > BSM-C: Comunicação <br/ > BSM-OD: Orientação ao Detalhe <br/ > BSM-PA: Proatividade <br/ > BSM-T: Trabalho em Equipe";
+
 	}
 	
+	@GetMapping("/meus_objetivos")
+	public String meus_objetivos() {
+		return 
+				
+				"Estudadar mais<br/ >"
+				+"Aperfeiçando mais<br/ >"+
+				"Aprendendo mais<br/ >"
+				+"Focar mais nos meus objetivos<br/ >";
+				
+	}
 }
